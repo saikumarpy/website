1cef38cadd20777210fca688ac0971219e2d78b3
saikumarpy <saikumarpy@gmail.com>
2017-01-27 18:19:34 -0500

    added database setup, Created models and activated models, Also created superuser as "admin" along with a new view

--- music/urls.py
+++ music/urls.py
@@ -3,4 +3,7 @@ from .import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    # /music/4564/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1> This is Music home page</h1>")
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
