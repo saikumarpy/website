7ca9e4656a5e0d66c5281fc760cd5938b19c9e38
saikumarpy <saikumarpy@gmail.com>
2017-01-30 12:41:20 -0500

    Designed Detai9ls Template

--- music/views.py
+++ music/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.http import HttpResponse
+from django.http import Http404
 from .models import  Album
 from django.shortcuts import render
+
+
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album})
