2d593f82386acec883b0832d9859d4169ff8f9ed
saikumarpy <saikumarpy@gmail.com>
2017-02-13 18:56:29 -0500

    Added User Authentication and login page(Registration page)

--- music/urls.py
+++ music/urls.py
@@ -6,6 +6,9 @@ app_name = 'music'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
 
+    # for user registration page
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     # /music/<album_id>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
--- music/views.py
+++ music/views.py
@@ -2,6 +2,10 @@ from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import Album
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -25,3 +29,39 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('index')
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    # display blank form
+    def get(self, request):
+        form=self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    # form for data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            #cleaned data ( normalized data)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # for changing user password
+            user.set_password(password)
+            user.save()
+
+            # returns user objects if credentials are correct (Authentication)
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('index')
+
+        return render(request, self.template_name, {'form': form})
